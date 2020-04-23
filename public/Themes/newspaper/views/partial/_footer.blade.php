7c657bbe1810521fbe3ed14d4e45d495f8c8a9fe
Ahmad Syarif <syarifahmad817@gmail.com>
2020-04-24 05:13:49 +0700

    update footer credit

--- public/Themes/newspaper/views/partial/_footer.blade.php
+++ public/Themes/newspaper/views/partial/_footer.blade.php
@@ -7,7 +7,7 @@
                     <div class="col-12">
                         <!-- Copywrite -->
                         <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy; {{date('Y')}} All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> and modified by <a href="#">Keeta CMS</a>
+Copyright &copy; {{date('Y')}} All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> and modified by <a href="https://github.com/syarifahmad817/keetacms">Keeta CMS</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                     </div>
                 </div>
