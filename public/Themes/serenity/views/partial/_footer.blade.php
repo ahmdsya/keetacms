51282eee3341182a95511d9c655a9150dfc3a565
Ahmad Syarif <syarifahmad817@gmail.com>
2020-04-24 05:12:50 +0700

    update footer credit

--- public/Themes/serenity/views/partial/_footer.blade.php
+++ public/Themes/serenity/views/partial/_footer.blade.php
@@ -4,7 +4,7 @@
         &copy; Copyright <strong><span>KeetaCMS</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> and modified by <a href="#">Keeta CMS</a>
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> and modified by <a href="https://github.com/syarifahmad817/keetacms">Keeta CMS</a>
       </div>
     </div>
   </footer>
